84ca157b6b7a801e1ab6fc13fa9f3b3397f0460a
Yaroslava <viktorova.yaroslava26@gmail.com>
2024-03-12 18:12:37 +0200

    practical lesson 1

--- practical_lesson_1_Yaroslava_Viktorova_ІДС-501.py
+++ practical_lesson_1_Yaroslava_Viktorova_ІДС-501.py
@@ -1,14 +1,14 @@
 import nltk
 from nltk.corpus import gutenberg
 import string
+
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 from nltk.text import Text
+
 nltk.download('gutenberg')
 nltk.download('punkt')
-
-
 
 # print(list(gutenberg.fileids()))
 # emma_text = gutenberg.raw('austen-emma.txt')
@@ -16,7 +16,7 @@ nltk.download('punkt')
 
 # по словам
 hamlet_w = gutenberg.words('shakespeare-hamlet.txt')
-print(hamlet_w )
+print(hamlet_w)
 
 # по реченням
 hamlet_s = gutenberg.sents('shakespeare-hamlet.txt')
@@ -28,13 +28,13 @@ print(hamlet_p)
 
 # Frequency Distributions
 # слова, які найчастіше зустрічаються в тексті (разом з пунктуацією)
-hamlet_dict = nltk.FreqDist(hamlet_w) #словник слово/скільки раз трапляється
+hamlet_dict = nltk.FreqDist(hamlet_w)  # словник слово/скільки раз трапляється
 print(hamlet_dict.most_common(15))
 
 print(string.punctuation)
 
 # відфільтруємо текст (прибираємо пунктуацію)
-hamlet_without_punct = [] #текст без пунктуації
+hamlet_without_punct = []  # текст без пунктуації
 for word in hamlet_w:
     if word not in string.punctuation:
         hamlet_without_punct.append(word)
@@ -42,7 +42,7 @@ for word in hamlet_w:
 print(len(hamlet_w))  # довжина тексту з пунктуацію
 print(len(hamlet_without_punct))  # довжина тексту без пунктуацію
 
-hamlet_dict_no_punct = nltk.FreqDist(hamlet_without_punct) #словник без пунктуації слово/скільки раз трапляється
+hamlet_dict_no_punct = nltk.FreqDist(hamlet_without_punct)  # словник без пунктуації слово/скільки раз трапляється
 print(hamlet_dict_no_punct.most_common(10))
 
 english_stopwords = stopwords.words('english')
@@ -60,20 +60,20 @@ print(hamlet_dict_no_stop_word.most_common(10))
 
 print(hamlet_dict_no_stop_word.tabulate(25))
 
-#графік частота слів, які найчастіше зустрічаються
+# графік частота слів, які найчастіше зустрічаються
 fig = plt.figure()
-plt.gcf().subplots_adjust(bottom=0.30) # to avoid x-ticks cut-off
+plt.gcf().subplots_adjust(bottom=0.30)  # to avoid x-ticks cut-off
 
 hamlet_dict_no_stop_word.plot(10)
 plt.tight_layout()
 
-#Text Objects
+# Text Objects
 hamlet_t = Text(hamlet_w)
 hamlet_t.concordance('Hamlet', lines=5)
-hamlet_t.collocations(num=5) #слова які найчастіше зустрічаються разом
+hamlet_t.collocations(num=5)  # слова які найчастіше зустрічаються разом
 
 fig_1 = plt.figure()
-plt.gcf().subplots_adjust(bottom=0.30) # to avoid x-ticks cut-off
+plt.gcf().subplots_adjust(bottom=0.30)  # to avoid x-ticks cut-off
 
 hamlet_t.dispersion_plot(['Lord', 'King', 'Hamlet'])
 plt.show()
