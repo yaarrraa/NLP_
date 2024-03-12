a1ad0b9be9046a7920dbc1fa2d6f9702af81ee0d
Yaroslava <viktorova.yaroslava26@gmail.com>
2024-03-12 17:14:11 +0200

    practical lesson 1

--- practical_lesson_1_Yaroslava_Viktorova_ІДС-501.py
+++ practical_lesson_1_Yaroslava_Viktorova_ІДС-501.py
@@ -1,14 +1,13 @@
 import nltk
 from nltk.corpus import gutenberg
 import string
-# nltk.download('stopwords')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 from nltk.text import Text
-# nltk.download('gutenberg')
-# nltk.download('punkt')
-from nltk.corpus import movie_reviews
-# nltk.download('movie_reviews')
+nltk.download('gutenberg')
+nltk.download('punkt')
+
 
 
 # print(list(gutenberg.fileids()))
@@ -23,7 +22,7 @@ print(hamlet_w )
 hamlet_s = gutenberg.sents('shakespeare-hamlet.txt')
 print(hamlet_s)
 
-# по парагрфам
+# по параграфам
 hamlet_p = gutenberg.paras('shakespeare-hamlet.txt')
 print(hamlet_p)
 
@@ -44,33 +43,37 @@ print(len(hamlet_w))  # довжина тексту з пунктуацію
 print(len(hamlet_without_punct))  # довжина тексту без пунктуацію
 
 hamlet_dict_no_punct = nltk.FreqDist(hamlet_without_punct) #словник без пунктуації слово/скільки раз трапляється
-print(hamlet_dict_no_punct.most_common(15))
+print(hamlet_dict_no_punct.most_common(10))
 
 english_stopwords = stopwords.words('english')
 print(english_stopwords[:10])
 
 hamlet_without_stop_word = []
 for word in hamlet_without_punct:
-    if word.lower not in english_stopwords:
+    if word.lower() not in english_stopwords:
         hamlet_without_stop_word.append(word)
 print(len(hamlet_without_punct))
 print(len(hamlet_without_stop_word))
 
 hamlet_dict_no_stop_word = nltk.FreqDist(hamlet_without_stop_word)
-print(hamlet_dict_no_stop_word.most_common(15))
+print(hamlet_dict_no_stop_word.most_common(10))
 
 print(hamlet_dict_no_stop_word.tabulate(25))
 
 #графік частота слів, які найчастіше зустрічаються
-# fig = plt.figure()
-# plt.gcf().subplots_adjust(bottom=0.30) # to avoid x-ticks cut-off
-#
-# hamlet_dict_no_stop_word.plot(20)
-# plt.tight_layout()
+fig = plt.figure()
+plt.gcf().subplots_adjust(bottom=0.30) # to avoid x-ticks cut-off
+
+hamlet_dict_no_stop_word.plot(10)
+plt.tight_layout()
 
 #Text Objects
 hamlet_t = Text(hamlet_w)
-# print(type(hamlet_t))
-hamlet_t.concordance('Ham', lines=5)
+hamlet_t.concordance('Hamlet', lines=5)
 hamlet_t.collocations(num=5) #слова які найчастіше зустрічаються разом
 
+fig_1 = plt.figure()
+plt.gcf().subplots_adjust(bottom=0.30) # to avoid x-ticks cut-off
+
+hamlet_t.dispersion_plot(['Lord', 'King', 'Hamlet'])
+plt.show()
